b06d9c182dee30c3cb89092e77bfe27f84a345b2
mdasifahamed <mdasifahamed89@yahoo.com>
2024-11-02 00:55:00 +0600

    HardhatConfig
    Configuration of hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomicfoundation/hardhat-verify");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    infura_sepolia: {
+      url: process.env.INFURA_SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
